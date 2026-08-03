b8bb8869923a2bc628a1a1042db5e918899b57c9
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-03 21:37:32 +0500

    Update page.jsx

--- src/app/courses/[slug]/page.jsx
+++ src/app/courses/[slug]/page.jsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";      
 import { coursesData } from "@/app/Assets/data";
 import { CourseCard } from "@/Components/CourseCard";
 
@@ -8,6 +9,7 @@ export default async function page({ params }) {
   const { courses } = coursesData;
 
   const pageData = courses.find((e) => e.slug == slug);
+   if (!pageData) notFound(); 
 
   return (
     <section className="hero-glow overflow-hidden px-8 pt-28 md:pt-25 flex flex-col gap-6 items-center justify-center font-sans md:pl-[18%] min-h-screen">
