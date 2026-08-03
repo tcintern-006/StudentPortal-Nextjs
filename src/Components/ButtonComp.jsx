26a55114101801dde906a8d56a0ba04fd9612498
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-03 20:52:00 +0500

    Update ButtonComp.jsx

--- src/Components/ButtonComp.jsx
+++ src/Components/ButtonComp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ButtonComp = ({text}) => {
   return (
-  <button className='border border-[#8080802f] p-2 px-4 rounded-md cursor-pointer hover:bg-[#8080802f] transition-all duration-300 active:scale-95'>
+  <button className='border border-[#808080a1] p-2 px-4 rounded-md cursor-pointer hover:bg-[#8080802f] transition-all duration-300 active:scale-95'>
   {text}
 </button>
   )
