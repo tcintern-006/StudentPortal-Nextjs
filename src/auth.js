dee07d4c997ff6ea4c7f5fad236f1ecff7c3d140
TCI-2026-006 | Muhammad Ammar <ammarakbar4600@gmail.com>
2026-08-13 15:14:50 +0500

    Create auth.js

---
+++ src/auth.js
@@ -0,0 +1,12 @@
+export function getToken() {
+    if (typeof window === 'undefined') return null;
+    return localStorage.getItem('token');
+}
+
+export function setToken(token) {
+    localStorage.setItem('token', token);
+}
+
+export function removeToken() {
+    localStorage.removeItem('token');
+}
